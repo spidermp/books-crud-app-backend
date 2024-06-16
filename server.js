aad292cb2ddde35fd265f64499c7610fc79d4a9e
spidermp <markap.dev@gmail.com>
2024-06-16 01:50:26 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+var corsOptions = {
+  origin: process.env.CORS_ORIGIN
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+// use if I need to drop and resync the db
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Mark's application." });
+});
+
+require("./app/routes/book.routes.js")(app); 
+
+// set port, listen for requests
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});

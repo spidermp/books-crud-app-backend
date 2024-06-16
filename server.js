5dc920738388a8f445f0457746d234cf3c126f83
spidermp <markap.dev@gmail.com>
2024-06-16 04:19:10 -0500

    Updated port config

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 require("./app/routes/book.routes.js")(app); 
 
 // set port, listen for requests
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
